e21da6307f1b5d5f0375393da2f21d494ae3f104
Georgy-p2ptaxi <gstarikov@p2ptaxi.com>
2017-11-28 14:37:03 +0300

    more comments

--- p2ptaxi_preico.sol
+++ p2ptaxi_preico.sol
@@ -5,15 +5,36 @@ ALL SOLD = 80% of TOTAL => investors
 team => 15%
 bonus => 5% 
 
+minFillUp = 0.01 ETH
+
 preIco
 [0 week: 1 week) => +45% (11-17 dec)
 [1 week: 3 week) => +40% (18-31 dec)
 [3 week: 5 week) => +35% (01-14 jan)
 [5 week: 7 week) => +30% (15-28 jan)
 [7 week: 9 week) => +25% (29-30 jan)
-
+softCap = 200 000 $
 (total < softCap) && (time >= 9 week) => stop ICO & enable refund
 
+ICO
+softCAP = 2 000 000 $
+hardCap = 8 000 000 $ 
+[0 week: 1 week) => +30% (19-26 jan)
+[1 week: 2 week) => +25% (27-02 feb)
+[2 week: 3 week) => +20% (03-09 feb)
+[3 week: 4 week) => +15% (10-16 feb)
+[4 week: 5 week) => +10% (17-19 feb)
+
+generate bonus tokens 
+bonus = (ico*0.05)/0.80
+
+team tokens freeaze -> 1 year (till 19-feb-2019)
+team tokens = (ico*0.15)/0.80
+
+auto pay via smartcontract
+payout_from - eth address to make payout (every year)
+payout_journal -> [year] -> {amount, payout address}
+payout method -> address, amount to payout
 */
 
 contract p2pTaxiPreIco {
